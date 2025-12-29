2ce242ef1ec2a3cd8c44a55c0c1ca597bef82935
andoxl <andoxl@proton.me>
2025-12-29 18:13:22 +0100

    modified:   ttlock-standalone/Dockerfile 	modified:   ttlock-standalone/addon/package.json 	modified:   ttlock-standalone/start.sh

--- ttlock-standalone/start.sh
+++ ttlock-standalone/start.sh
@@ -1,40 +1,25 @@
 #!/usr/bin/env sh
 
-#
-# ---- ALL ENV variables are passed via docker-compose.yaml file ----
-#
-#export MQTT_HOST="192.168.2.122"
-#export MQTT_PORT=1883
-#export MQTT_SSL=0
-#export MQTT_USER="test1"
-#export MQTT_PASS="test1"
-#export GATEWAY= 
-#export GATEWAY_HOST= 
-#export GATEWAY_PORT= 
-#export GATEWAY_KEY="f8b55c272eb007f501560839be1f1e7e" 
-#export GATEWAY_USER="admin" 
-#export GATEWAY_PASS="admin" 
+export GATEWAY="none"
+export GATEWAY_HOST=""
+export GATEWAY_PORT=""
+export GATEWAY_KEY=""
+export GATEWAY_USER=""
+export GATEWAY_PASS=""
 
-# if (bashio::config.true "ignore_crc"); then
-#  echo "IGNORE CRC TRUE"
-#  export TTLOCK_IGNORE_CRC=1
-# fi
-# if (bashio::config.equals "gateway" "noble"); then
-#  echo "Disable noble auto-binding"
-#  export NOBLE_WEBSOCKET=1
-# fi
-# if (bashio::config.true "debug_communication"); then
-#  echo "Debug communication ON"
-#  export TTLOCK_DEBUG_COMM=1
-# fi
-# if (bashio::config.true "debug_mqtt"); then
-#  echo "Debug MQTT"
-#  export MQTT_DEBUG=1
-# fi
-# if (bashio::config.true "gateway_debug"); then
-#  echo "Debug gateway"
-#  export WEBSOCKET_DEBUG=1
-# fi
+export NOBLE_WEBSOCKET=1
+export WEBSOCKET_DEBUG=1
+
+/sbin/openrc
+touch /run/openrc/softlevel
+
+cd /etc/init.d
+
+echo Starting dbus
+./dbus start
+
+echo Starting bluetooth
+./bluetooth start
 
 cd /app
 npm start
