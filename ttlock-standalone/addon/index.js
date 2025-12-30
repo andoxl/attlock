18a09a5941b186198906597c2236df4dc36ace25
andoxl <andoxl@proton.me>
2025-12-30 12:44:05 +0100

    Update index.js

--- ttlock-standalone/addon/index.js
+++ ttlock-standalone/addon/index.js
@@ -23,6 +23,8 @@ init({
   gateway_key: process.env.GATEWAY_KEY,
   gateway_user: process.env.GATEWAY_USER,
   gateway_pass: process.env.GATEWAY_PASS,
+  localIP: process.env.FRONT_IP,
+  port: process.env.FRONT_PORT || 55099,
   // --- HTTP AUTH user&pass + fallback to `admin`:`admin`
   http_user: process.env.HTTP_USER || "admin",
   http_passwd: process.env.HTTP_PASS || "admin"
